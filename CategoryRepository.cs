a22d48c91fd5d2effb531ddaf0ac1ea75a6701d9
Serkan Kumru <serkankumru1997@gmail.com>
2023-09-14 17:41:15 +0300

    Add files via upload

---
+++ CategoryRepository.cs
@@ -0,0 +1,28 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Runtime.Caching;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace DAL
+{
+    public class CategoryRepository: BaseRepository<Catagory>
+    {
+        public override List<Catagory> List()
+        {
+            MemoryCache mChache = MemoryCache.Default;
+            List<Catagory> news = new List<Catagory>();
+            if (!mChache.Contains("catList"))
+            {
+                news = new NewsEntities1().Set<Catagory>().ToList();
+                mChache.Add("catList", news, DateTimeOffset.Now.AddSeconds(30));
+            }
+            else
+            {
+                news = (List<Catagory>)mChache.Get("catList");
+            }
+            return news;
+        }
+    }
+}
